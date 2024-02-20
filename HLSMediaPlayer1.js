ae17abb0174fe2b5f78744497e56d67e139db2c3
Ken V <67239145+Kennethv123@users.noreply.github.com>
2024-02-20 17:19:36 -0600

    test

--- HLSMediaPlayer1.js
+++ HLSMediaPlayer1.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var HlsVideo = document.getElementById("videofirst");
+    var HlsVideo = document.getElementById("videoa");
     var HlsVideoSrc = "assets/Master.m3u8";
     const defaultOptions = {};
 
