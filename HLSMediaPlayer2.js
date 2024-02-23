7bb23a02af7a62f0534d9e8f3d32a7cde97f824a
Ken V <67239145+Kennethv123@users.noreply.github.com>
2024-02-22 19:13:56 -0600

    test

--- HLSMediaPlayer2.js
+++ HLSMediaPlayer2.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     var HlsVideo = document.getElementById("Bvideo");
     var HlsVideoSrc = "assets/Master.m3u8";
+
+    
     const defaultOptions = {};
 
     if (Hls.isSupported()) {
